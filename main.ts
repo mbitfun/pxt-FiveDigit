fbe4efe836e43442090340f4e65642236ab3ab75
yusukeOteki <ut37186443@gmail.com>
2018-08-02 08:28:30 +0900

    Initial commit

---
+++ main.ts
@@ -0,0 +1,13 @@
+//% weight=70 icon="\uf1ec" color=#00bbd7
+namespace fiveDigit {
+    //% blockId=show_number block="Show a number %num"
+    export function showNumber(num: number = 0): void {
+        let str = num.toString()
+        for (let x = 0; x < 5; x++) {
+            let p = x - 5 + str.length
+            let d = p < 0 ? 0 : parseInt(str.substr(p, 1))
+            for (let y = 0; y < 5; y++)
+                (y > 0 ? (d - 5 * (d < 5 ? 0 : 1) >= y) : (d > 4)) ? led.plot(x, y) : led.unplot(x, y)
+        }
+    }
+}
---
+++ test.ts
@@ -0,0 +1,3 @@
+basic.forever(() => {
+  fiveDigit.showNumber(59273)
+})
