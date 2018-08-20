cda377f520993efa2184fc3c2111753666f80b29
yusukeOteki <ut37186443@gmail.com>
2018-08-20 16:36:15 +0900

    over flow

--- main.ts
+++ main.ts
@@ -3,11 +3,14 @@ namespace fiveDigit {
     //% blockId=show_number block="Show a number %num"
     export function showNumber(num: number = 0): void {
         let str = parseInt(num.toString()).toString()
-        for (let x = 0; x < 5; x++) {
-            let p = x - 5 + str.length
-            let d = p < 0 ? 0 : parseInt(str.substr(p, 1))
-            for (let y = 0; y < 5; y++)
-                (y > 0 ? (d - 5 * (d < 5 ? 0 : 1) >= 5 - y) : (d > 4)) ? led.plot(x, y) : led.unplot(x, y)
-        }
+        if (num > 99999)
+            basic.showString("E")
+        else
+            for (let x = 0; x < 5; x++) {
+                let p = x - 5 + str.length
+                let d = p < 0 ? 0 : parseInt(str.substr(p, 1))
+                for (let y = 0; y < 5; y++)
+                    (y > 0 ? (d - 5 * (d < 5 ? 0 : 1) >= 5 - y) : (d > 4)) ? led.plot(x, y) : led.unplot(x, y)
+            }
     }
 }
